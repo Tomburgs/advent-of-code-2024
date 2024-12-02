ba1a3f51e7e074dd977e0061324dd74ef7236c13
Toms Burgmanis <toms.burgmanis@gmail.com>
2024-12-02 21:07:11 +0100

    feat(day1): add part 2

--- day1/app/Main.hs
+++ day1/app/Main.hs
@@ -1,6 +1,8 @@
 module Main where
 
+import qualified Data.HashMap.Strict as HashMap
 import Data.List
+import Data.Maybe (fromMaybe)
 
 createLists :: FilePath -> IO ([Int], [Int])
 createLists file = do
@@ -27,9 +29,16 @@ main :: IO ()
 main = do
   (l1, l2) <- createLists "./data/lists.txt"
 
+  -- Part 1:
   let sl1 = sort l1
   let sl2 = sort l2
 
   let distance = calculateDistance sl1 sl2
 
-  print distance
+  print $ "Distance: " ++ show distance
+
+  -- Part 2:
+  let occurrences = foldr (\v acc -> HashMap.insertWith (+) v 1 acc) HashMap.empty l2
+  let similarity = foldr (\v acc -> acc + (v * (fromMaybe 0 (HashMap.lookup v occurrences)))) 0 l1
+
+  print $ "Similarity: " ++ show similarity
