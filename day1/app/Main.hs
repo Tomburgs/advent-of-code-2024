46a048762d00a951139256b6c23e525f7e18287d
Toms Burgmanis <toms.burgmanis@gmail.com>
2024-12-03 10:06:48 +0100

    feat(day1): simplify calculate distance

--- day1/app/Main.hs
+++ day1/app/Main.hs
@@ -18,13 +18,6 @@ createLists file = do
       (list1, list2) = unzip parsed
   return (list1, list2)
 
-calculateDistance :: [Int] -> [Int] -> Int
-calculateDistance list1 list2 = go list1 list2 0
-  where
-    go [] [] acc = acc
-    go (a : ar) (b : br) acc = go ar br (acc + abs (a - b))
-    go _ _ _ = error "Lists must have the same length"
-
 main :: IO ()
 main = do
   (l1, l2) <- createLists "./data/lists.txt"
@@ -32,8 +25,7 @@ main = do
   -- Part 1:
   let sl1 = sort l1
   let sl2 = sort l2
-
-  let distance = calculateDistance sl1 sl2
+  let distance = sum $ zipWith (\a b -> abs (a - b)) sl1 sl2
 
   print $ "Distance: " ++ show distance
 
